ff56957c7d000ecf356d2bb0de6674a3760146ef
Rahma <Rahma@gmail.com>
2025-07-26 23:51:48 +0300

    update deprecated packages

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
 import withPWAInit from "next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // يعمل فقط في وضع الإنتاج
+  disable: process.env.NODE_ENV === "development", // يعمل فقط في الـ production
 });
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default withPWA(nextConfig);
